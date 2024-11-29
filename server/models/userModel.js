9cbd38011aee41a72479d9160e9c026ef627e48f
adarsh02 <aadarshraazz007@gmail.com>
2024-11-29 18:39:56 +0530

    added method for jwtAuth,comparePassword and generating hashed password

--- server/models/userModel.js
+++ server/models/userModel.js
@@ -1,4 +1,6 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const userSchema = new mongoose.Schema({
   fullname: {
@@ -22,8 +24,31 @@ const userSchema = new mongoose.Schema({
   password: {
     type: String,
     required: true,
+    select: false,
   },
   socketId: {
     type: String,
   },
 });
+
+userSchema.methods.generateAuthToken = function () {
+  const token = jwt.sign(
+    {
+      _id: this._id,
+    },
+    process.env.JWT_SECRET
+  );
+  return token;
+};
+
+userSchema.methods.comparePassword = async function () {
+  return await bcrypt.compare(password, this.password);
+};
+
+userSchema.static.hashPassword = async function (password) {
+  return await bcrypt.hash(password, 10);
+};
+
+const userModel = mongoose.model("user", userSchema);
+
+module.exports = userModel;
