bb812cde0076ede1b7c6c4d1878eca505b04dd48
adarsh02 <aadarshraazz007@gmail.com>
2024-11-29 15:05:16 +0530

    connected to database

--- server/app.js
+++ server/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const connectToDB = require("./db/db");
 
 dotenv.config();
 const cors = require("cors");
 
 const app = express();
 app.use(cors());
+connectToDB();
 
 module.exports = app;
--- server/db/db.js
+++ server/db/db.js
@@ -2,10 +2,7 @@ const mongoose = require("mongoose");
 
 const connectToDB = () => {
   mongoose
-    .connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    })
+    .connect(process.env.MONGO_URI)
     .then(() => console.log("Connected to database"))
     .catch((err) => console.error("Database connection error:", err));
 };
