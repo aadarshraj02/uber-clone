b1b17e6cf70253e1b1da88aeae7357434b755201
adarsh02 <aadarshraazz007@gmail.com>
2024-11-29 19:32:37 +0530

    added function to create new user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,7 +1,37 @@
 const userModel = require("../models/userModel");
+const userService = require("../services/userService");
+const { validationResult } = require("express-validator");
 
 const registerUser = async (req, res, next) => {
-    
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).json({
+      errors: errors.array(),
+    });
+  }
+
+  try {
+    const { firstName, lastName, email, password } = req.body;
+
+    const hashedPassword = await userModel.hashedPassword(password);
+
+    const user = await userService.createUser({
+      firstName,
+      lastName,
+      email,
+      password: hashedPassword,
+    });
+    const token = user.generateAuthToken();
+    res.status(201).json({
+      token,
+      user,
+    });
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
 };
 
 module.exports = {
