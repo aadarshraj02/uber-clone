b79e1dda2166f10834c334aac3b236aa8cc8ff1c
adarsh02 <aadarshraazz007@gmail.com>
2024-11-30 02:01:08 +0530

    added middleware authcaptain

--- server/middlewares/authMiddleware.js
+++ server/middlewares/authMiddleware.js
@@ -1,4 +1,5 @@
 const userModel = require("../models/userModel");
+const captainModel = require("../models/captainModel");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
@@ -30,4 +31,32 @@ const authUser = async (req, res, next) => {
   }
 };
 
-module.exports = { authUser };
+const authCaptain = async (req, res, next) => {
+  const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+  if (!token)
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+
+  const isBlacklisted = await captainModel.findOne({ token: token });
+
+  if (isBlacklisted) {
+    return res.status(401).json({
+      message: "Unauthorized",
+    });
+  }
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const captain = await userModel.findById(decoded._id);
+    req.captain = captain;
+
+    return next();
+  } catch (error) {
+    return res.status(500).json({
+      message: error.message,
+    });
+  }
+};
+
+module.exports = { authUser, authCaptain };
