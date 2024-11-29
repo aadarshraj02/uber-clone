fe9460fb7943cc8ac49e57db8e758d23c73fb597
adarsh02 <aadarshraazz007@gmail.com>
2024-11-29 19:01:04 +0530

    added routes for register with express-validator

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,4 +1,24 @@
 const express = require("express");
+const { body } = require("express-validator");
+const userController = require("../controllers/userController");
+
 const router = express.Router();
+router.post(
+  "/register",
+  [
+    body("email").isEmail().withMessage("Invalid Email"),
+    body("fullname.firstName")
+      .isLength({
+        min: 3,
+      })
+      .withMessage("First Name must be of 3 character long"),
+    body("password")
+      .isLength({
+        min: 5,
+      })
+      .withMessage("Password must be of 5 character long"),
+  ],
+  userController
+);
 
 module.exports = router;
