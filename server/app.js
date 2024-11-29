7bc6ffb43abda8b81a8c27c88b1e307c4a45b156
adarsh02 <aadarshraazz007@gmail.com>
2024-11-29 14:46:51 +0530

    setup env cors and server

--- server/app.js
+++ server/app.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 
 module.exports = app;
