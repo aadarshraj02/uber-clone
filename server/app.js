50b82c79cfce0db835361483c37fdc44c596105f
adarsh02 <aadarshraazz007@gmail.com>
2024-11-29 19:35:28 +0530

    added user routes in app.js

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const connectToDB = require("./db/db");
+const userRoutes = require("./routes/userRoutes");
 
 dotenv.config();
 const cors = require("cors");
@@ -8,5 +9,14 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 connectToDB();
+
+app.use(express.json());
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
+
+app.use("/api/users", userRoutes);
 
 module.exports = app;
