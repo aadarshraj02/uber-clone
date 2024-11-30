5cfa732352309ae6fc2667f861d6a08160477121
adarsh02 <aadarshraazz007@gmail.com>
2024-11-30 20:54:59 +0530

    added routes for login and signup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,22 @@
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import CaptainLogin from "./pages/CaptainLogin";
+import CaptainSignup from "./pages/CaptainSignup";
+
 const App = (): JSX.Element => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/captain-login" element={<CaptainLogin />} />
+        <Route path="/captain-signup" element={<CaptainSignup />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
