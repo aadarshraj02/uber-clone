cef96d9f978e37e72d7d5439ecb8ce47e2f27ab3
adarsh02 <aadarshraazz007@gmail.com>
2024-12-01 17:14:39 +0530

    updated login page ui

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -4,6 +4,8 @@ import { useState } from "react";
 const Login = (): JSX.Element => {
   const [emailFocused, setEmailFocused] = useState(false);
   const [passwordFocused, setPasswordFocused] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   return (
     <div className="h-screen flex flex-col">
@@ -12,70 +14,73 @@ const Login = (): JSX.Element => {
           Uber
         </Link>
       </div>
-      <div className="flex flex-grow sm:items-center justify-center">
-        <form className="rounded-md p-8 w-full max-w-md space-y-6" action="">
-          <div>
-            <label htmlFor="email" className="block text-xl mb-2">
-              What's your email?
-            </label>
-            <input
-              id="email"
-              type="email"
-              required
-              placeholder="email@example.com"
-              className={`w-full px-4 py-2 rounded-md border transition-colors duration-300 focus:outline-none ${
-                emailFocused ? "bg-white" : "bg-zinc-200"
-              }`}
-              onFocus={() => setEmailFocused(true)}
-              onBlur={() => setEmailFocused(false)}
-            />
+      <div className="flex flex-grow sm:items-center mt-5 sm:mt-0 justify-center">
+        <div className="w-full max-w-md">
+          <form className="rounded-md px-8 space-y-6" action="">
+            <div>
+              <label htmlFor="email" className="block text-xl mb-2">
+                What's your email?
+              </label>
+              <input
+                id="email"
+                type="email"
+                value={email}
+                required
+                placeholder="email@example.com"
+                className={`w-full px-4 py-2 rounded-md border transition-colors duration-300 focus:outline-none ${
+                  emailFocused ? "bg-white" : "bg-zinc-200"
+                }`}
+                onFocus={() => setEmailFocused(true)}
+                onBlur={() => setEmailFocused(false)}
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div>
+              <label htmlFor="password" className="block text-xl mb-2">
+                Enter your password
+              </label>
+              <input
+                id="password"
+                type="password"
+                value={password}
+                required
+                placeholder="Your password"
+                className={`w-full px-4 py-2 rounded-md border transition-colors duration-300 focus:outline-none ${
+                  passwordFocused ? "bg-white" : "bg-zinc-200"
+                }`}
+                onFocus={() => setPasswordFocused(true)}
+                onBlur={() => setPasswordFocused(false)}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <button className="w-full bg-black text-white py-2 rounded-md hover:bg-gray-800 transition-colors">
+              Login
+            </button>
+          </form>
+          <div className="rounded-md px-8 space-y-4">
+            <div className="text-center">
+              <Link
+                to="/signup"
+                className="text-blue-600 hover:underline text-sm"
+              >
+                New here? Create an account
+              </Link>
+            </div>
+            <p className="text-xs text-gray-500 mt-4">
+              By proceeding, you consent to get calls, WhatsApp, or SMS/RCS
+              messages, including by automated means, from Uber and its
+              affiliates to the number provided.
+            </p>
+            <div className="mt-4 text-center">
+              <Link
+                to="/captain-login"
+                className="inline-block w-full bg-gray-300 text-black px-4 py-2 rounded-md hover:bg-gray-400 transition-colors"
+              >
+                Sign in as Captain
+              </Link>
+            </div>
           </div>
-          <div>
-            <label htmlFor="password" className="block text-xl mb-2">
-              Enter your password
-            </label>
-            <input
-              id="password"
-              type="password"
-              required
-              placeholder="Your password"
-              className={`w-full px-4 py-2 rounded-md border transition-colors duration-300 focus:outline-none ${
-                passwordFocused ? "bg-white" : "bg-zinc-200"
-              }`}
-              onFocus={() => setPasswordFocused(true)}
-              onBlur={() => setPasswordFocused(false)}
-            />
-          </div>
-          <button
-            type="submit"
-            className="w-full bg-black text-white py-2 rounded-md hover:bg-gray-800 transition-colors"
-          >
-            Login
-          </button>
-          <div className="text-center">
-            <Link
-              to="/signup"
-              className="text-blue-600 hover:underline text-sm"
-            >
-              New here? Create an account
-            </Link>
-          </div>
-
-          <p className="text-xs text-gray-500 mt-4">
-            By proceeding, you consent to get calls, WhatsApp, or SMS/RCS
-            messages, including by automated means, from Uber and its affiliates
-            to the number provided.
-          </p>
-
-          <div className="mt-4 text-center">
-            <Link
-              to="/captain-login"
-              className="inline-block w-full bg-gray-300 text-black px-4 py-2 rounded-md hover:bg-gray-400 transition-colors"
-            >
-              Sign in as Captain
-            </Link>
-          </div>
-        </form>
+        </div>
       </div>
     </div>
   );
