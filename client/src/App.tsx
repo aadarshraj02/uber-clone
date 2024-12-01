6ee862d3767627e3ba6c683932b74bf0851452a1
adarsh02 <aadarshraazz007@gmail.com>
2024-12-02 00:58:31 +0530

    imported and added route for Home page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,12 +4,14 @@ import Signup from "./pages/Signup";
 import CaptainLogin from "./pages/CaptainLogin";
 import CaptainSignup from "./pages/CaptainSignup";
 import Start from "./pages/Start";
+import Home from "./pages/Home";
 
 const App = (): JSX.Element => {
   return (
     <div>
       <Routes>
         <Route path="/" element={<Start />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/captain-login" element={<CaptainLogin />} />
