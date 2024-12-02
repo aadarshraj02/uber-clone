76fc22e5226ca4846b8eef315acc383ad8421eb1
adarsh02 <aadarshraazz007@gmail.com>
2024-12-02 15:43:34 +0530

    added logout

--- client/src/hooks/useUserLogout.ts
+++ client/src/hooks/useUserLogout.ts
@@ -1,1 +1,32 @@
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
+const useLogout = () => {
+  const navigate = useNavigate();
+
+  const logout = async () => {
+    const token = localStorage.getItem("token");
+
+    try {
+      const response = await axios.get(
+        `${import.meta.env.VITE_API_URL}/users/logout`,
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        }
+      );
+
+      if (response.status === 200) {
+        localStorage.removeItem("token");
+        navigate("/login");
+      }
+    } catch (err) {
+      console.error("Logout failed", err);
+    }
+  };
+
+  return logout;
+};
+
+export default useLogout;
--- client/src/pages/Userlogout.tsx
+++ client/src/pages/Userlogout.tsx
@@ -1,5 +1,14 @@
-const Userlogout = () => {
-  return <div>Userlogout</div>;
+import { useEffect } from "react";
+import useLogout from "../hooks/useUserLogout";
+
+const UserLogout = () => {
+  const logout = useLogout();
+
+  useEffect(() => {
+    logout();
+  }, [logout]);
+
+  return <div>UserLogout</div>;
 };
 
-export default Userlogout;
+export default UserLogout;
