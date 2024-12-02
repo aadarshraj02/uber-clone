560a81512400c3fb819f111dc663784f0bae0307
adarsh02 <aadarshraazz007@gmail.com>
2024-12-02 16:46:29 +0530

    updated layout for captain signup

--- client/src/pages/CaptainSignup.tsx
+++ client/src/pages/CaptainSignup.tsx
@@ -6,8 +6,10 @@ const CaptainSignup = (): JSX.Element => {
   const [password, setPassword] = useState("");
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
-  const [captainData, setCaptainData] = useState({});
-  const [focusedField, setFocusedField] = useState("");
+  const [vehicleColor, setVehicleColor] = useState("");
+  const [vehiclePlate, setVehiclePlate] = useState("");
+  const [vehicleCapacity, setVehicleCapacity] = useState("");
+  const [vehicleType, setVehicleType] = useState("");
 
   const submitHandler = (e: React.FormEvent) => {
     e.preventDefault();
@@ -18,23 +20,32 @@ const CaptainSignup = (): JSX.Element => {
       },
       email: email,
       password: password,
+      vehicle: {
+        color: vehicleColor,
+        plate: vehiclePlate,
+        capacity: vehicleCapacity,
+        vehicleType: vehicleType,
+      },
     };
-    setCaptainData(newUser);
-    console.log(captainData);
+    console.log(newUser);
     setFirstName("");
     setLastName("");
     setEmail("");
     setPassword("");
+    setVehicleColor("");
+    setVehiclePlate("");
+    setVehicleCapacity("");
+    setVehicleType("");
   };
 
   return (
-    <div className="h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col">
       <div className="bg-black text-white px-8 py-4 text-center sm:text-left">
         <Link to="/" className="px-4 text-3xl">
           Uber Captain
         </Link>
       </div>
-      <div className="flex flex-grow sm:items-center mt-5 sm:mt-0 justify-center">
+      <div className="flex flex-grow sm:items-center my-5 justify-center">
         <div className="w-full max-w-md">
           <form className="rounded-md px-8 space-y-6" onSubmit={submitHandler}>
             <div>
@@ -48,11 +59,7 @@ const CaptainSignup = (): JSX.Element => {
                   value={firstName}
                   required
                   placeholder="First Name"
-                  className={`w-full px-4 py-2 rounded-md border transition-colors duration-300 focus:outline-none ${
-                    focusedField === "firstName" ? "bg-white" : "bg-zinc-200"
-                  }`}
-                  onFocus={() => setFocusedField("firstName")}
-                  onBlur={() => setFocusedField("")}
+                  className="w-full px-4 py-2 rounded-md border bg-zinc-200 focus:bg-white"
                   onChange={(e) => setFirstName(e.target.value)}
                 />
                 <input
@@ -61,11 +68,7 @@ const CaptainSignup = (): JSX.Element => {
                   value={lastName}
                   required
                   placeholder="Last Name"
-                  className={`w-full px-4 py-2 rounded-md border transition-colors duration-300 focus:outline-none ${
-                    focusedField === "lastName" ? "bg-white" : "bg-zinc-200"
-                  }`}
-                  onFocus={() => setFocusedField("lastName")}
-                  onBlur={() => setFocusedField("")}
+                  className="w-full px-4 py-2 rounded-md border bg-zinc-200 focus:bg-white"
                   onChange={(e) => setLastName(e.target.value)}
                 />
               </div>
@@ -80,11 +83,7 @@ const CaptainSignup = (): JSX.Element => {
                 value={email}
                 required
                 placeholder="email@example.com"
-                className={`w-full px-4 py-2 rounded-md border transition-colors duration-300 focus:outline-none ${
-                  focusedField === "email" ? "bg-white" : "bg-zinc-200"
-                }`}
-                onFocus={() => setFocusedField("email")}
-                onBlur={() => setFocusedField("")}
+                className="w-full px-4 py-2 rounded-md border bg-zinc-200 focus:bg-white"
                 onChange={(e) => setEmail(e.target.value)}
               />
             </div>
@@ -98,13 +97,73 @@ const CaptainSignup = (): JSX.Element => {
                 value={password}
                 required
                 placeholder="Your password"
-                className={`w-full px-4 py-2 rounded-md border transition-colors duration-300 focus:outline-none ${
-                  focusedField === "password" ? "bg-white" : "bg-zinc-200"
-                }`}
-                onFocus={() => setFocusedField("password")}
-                onBlur={() => setFocusedField("")}
+                className="w-full px-4 py-2 rounded-md border bg-zinc-200 focus:bg-white"
                 onChange={(e) => setPassword(e.target.value)}
               />
+            </div>
+            <div>
+              <label htmlFor="vehiclePlate" className="block text-xl mb-2">
+                Vehicle Plate Number
+              </label>
+              <input
+                id="vehiclePlate"
+                type="text"
+                value={vehiclePlate}
+                required
+                placeholder="e.g., ABC-1234"
+                className="w-full px-4 py-2 rounded-md border bg-zinc-200 focus:bg-white"
+                onChange={(e) => setVehiclePlate(e.target.value)}
+              />
+            </div>
+            <div className="flex flex-col sm:flex-row gap-2">
+              <div className="flex-1">
+                <label htmlFor="vehicleColor" className="block text-xl mb-2">
+                  Vehicle Color
+                </label>
+                <input
+                  id="vehicleColor"
+                  type="text"
+                  value={vehicleColor}
+                  required
+                  placeholder="e.g., Red"
+                  className="w-full px-4 py-2 rounded-md border bg-zinc-200 focus:bg-white"
+                  onChange={(e) => setVehicleColor(e.target.value)}
+                />
+              </div>
+              <div className="flex-1">
+                <label htmlFor="vehicleCapacity" className="block text-xl mb-2">
+                  Vehicle Capacity
+                </label>
+                <input
+                  id="vehicleCapacity"
+                  type="number"
+                  value={vehicleCapacity}
+                  required
+                  min={1}
+                  placeholder="e.g., 4"
+                  className="w-full px-4 py-2 rounded-md border bg-zinc-200 focus:bg-white appearance-none"
+                  onChange={(e) => setVehicleCapacity(e.target.value)}
+                />
+              </div>
+            </div>
+            <div>
+              <label htmlFor="vehicleType" className="block text-xl mb-2">
+                Vehicle Type
+              </label>
+              <select
+                id="vehicleType"
+                value={vehicleType}
+                required
+                className="w-full px-4 py-2 rounded-md border bg-zinc-200 focus:bg-white"
+                onChange={(e) => setVehicleType(e.target.value)}
+              >
+                <option value="" disabled>
+                  Select Vehicle Type
+                </option>
+                <option value="Car">🚗 Car</option>
+                <option value="Bike">🏍️ Bike</option>
+                <option value="Auto">🛺 Auto</option>
+              </select>
             </div>
             <button className="w-full bg-black text-white py-2 rounded-md hover:bg-gray-800 transition-colors">
               Sign Up
@@ -119,7 +178,7 @@ const CaptainSignup = (): JSX.Element => {
                 Existing Captain? Login here
               </Link>
             </div>
-            <p className="text-xs text-gray-500 mt-4">
+            <p className="text-xs text-gray-500 my-16">
               By proceeding, you consent to get calls, WhatsApp, or SMS/RCS
               messages, including by automated means, from Uber and its
               affiliates to the number provided.
