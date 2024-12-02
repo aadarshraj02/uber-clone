eabb51f876669b2959f983dccbaaadfec68009fa
adarsh02 <aadarshraazz007@gmail.com>
2024-12-02 16:20:05 +0530

    added captain data context

--- client/src/context/CaptainContext.tsx
+++ client/src/context/CaptainContext.tsx
@@ -1,7 +1,23 @@
-const CaptainContext = () => {
-  return (
-    <div>CaptainContext</div>
-  )
-}
+import { createContext, useState } from "react";
 
-export default CaptainContext
+export const CaptainDataContext = createContext<any>({
+  user: { email: "", fullname: { firstName: "", lastName: "" } },
+  setCaptain: () => {},
+});
+
+const CaptainContext = ({ children }: any) => {
+  const [captain, setCaptain] = useState({
+    email: "",
+    fullname: {
+      firstName: "",
+      lastName: "",
+    },
+  });
+  return (
+    <CaptainDataContext.Provider value={{ captain, setCaptain }}>
+      {children}
+    </CaptainDataContext.Provider>
+  );
+};
+
+export default CaptainContext;
